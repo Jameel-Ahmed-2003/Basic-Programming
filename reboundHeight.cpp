c64f664fc8d9f27ea91cedd1880d8ccc03053a0c
Jameel Ahmed. J <123755027+Jameel-Ahmed-2003@users.noreply.github.com>
2024-08-27 21:35:00 +0530

    Create reboundHeight.cpp

---
+++ reboundHeight.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int v,vn,h1;
+    cin>>h1>>v>>vn;
+    int ans=h1*pow((v/vn),2);
+    cout<<ans;
+    return 0;
+}
