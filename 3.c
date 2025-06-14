0a0b3cd5b9f7a176ce1d6a565aed522ca576f84c
Jameel Ahmed. J <123755027+Jameel-Ahmed-2003@users.noreply.github.com>
2025-06-14 14:02:58 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -8,19 +8,25 @@ output 2 : dog lazy a over jumps fox brown quick The
 */
 #include<stdio.h>
 #include<string.h>
-#define MAX_LENGTH 100
-char str[MAX_LENGTH];
-int i;
+#define MAXLENGTH 100
+char*words[MAXLENGTH];
+char result[MAXLENGTH]="";
+int i=0;
 void reverseWords(char*str)
 {
-    char*words[MAX_LENGTH];
     for(char*portion=strtok(str," ");portion!=NULL;words[i++]=portion,portion=strtok(NULL," "));
     for(i-=1;i>=0;i--)
-    printf("%s ",words[i]);
+    {
+        strcat(result,words[i]);
+        if(i!=0)
+        strcat(result," ");
+    }
 }
 void main()
 {
+    char str[MAXLENGTH];
     printf("Enter a string : ");
     scanf("%[^\n]s",str);
     reverseWords(str);
+    printf("%s",result);
 }
